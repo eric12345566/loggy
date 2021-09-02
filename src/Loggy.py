e1255d952a86dddbcbcc2d100269b5b015ed8ac2
Eric Shih <eric12345566@gmail.com>
2021-09-02 17:20:09 +0800

    feat: Static attribute for setup global setting for loggy

--- src/Loggy.py
+++ src/Loggy.py
@@ -4,11 +4,27 @@ import time
 
 
 class Loggy:
-    def __init__(self, module_name=None, theme=ClassicTheme, show_log_time=False):
-        self.__theme = theme()
+    _theme = ClassicTheme
+    _show_log_time = True
+    _log_time_struct = "%m/%d %H:%M:%S"
+
+    def __init__(self, module_name=None, theme=None, show_log_time=None, log_time_struct=None):
+        if theme is not None:
+            self.__theme = theme()
+        else:
+            self.__theme = Loggy._theme()
+
+        if show_log_time is not None:
+            self.__show_log_time = show_log_time
+        else:
+            self.__show_log_time = Loggy._show_log_time
+
         self.__module_name = module_name
-        self.__show_log_time = show_log_time
-        self.__log_time_struct = "%m/%d %H:%M:%S"
+
+        if log_time_struct is not None:
+            self.__log_time_struct = log_time_struct
+        else:
+            self.__log_time_struct = Loggy._log_time_struct
 
     @staticmethod
     def __print_log(log_text):
@@ -17,11 +33,32 @@ class Loggy:
     def set_theme(self, loggyTheme):
         self.__theme = loggyTheme()
 
-    def set_log_time(self, log_time):
+    def set_show_log_time(self, log_time):
         self.__show_log_time = log_time
 
-    def set_log_time_struct(self, time_struct):
-        self.__log_time_struct = time_struct
+    @classmethod
+    def class_set_log_time_struct(cls, time_struct):
+        cls._log_time_struct = time_struct
+
+    @classmethod
+    def class_get_log_time_struct(cls):
+        return cls._log_time_struct
+
+    @classmethod
+    def class_set_show_log_time(cls, show_log_time):
+        cls._show_log_time = show_log_time
+
+    @classmethod
+    def class_get_show_log_time(cls):
+        return cls._show_log_time
+
+    @classmethod
+    def class_set_theme(cls, theme):
+        cls._theme = theme
+
+    @classmethod
+    def class_get_theme(cls):
+        return cls._theme
 
     def info(self, log, *texts, name=None):
         log_time = time.localtime()
@@ -90,6 +127,7 @@ class Loggy:
 
 
 if __name__ == '__main__':
+    Loggy.class_set_show_log_time(False)
     logger = Loggy("Loggy Test", show_log_time=True)
     # logger.set_theme(ClassicTheme)
     logger.info("This is the info", " hello")
@@ -97,3 +135,6 @@ if __name__ == '__main__':
     logger.warning("This is the info")
     logger.error("This is the info")
     logger.critical("This is the info")
+
+    logger2 = Loggy("Loggy2")
+    logger2.info("hello")
